8f928d0b1539d5f3cd6d08a1418c52db44d937b2
AbdullahMelihYurtkap <99017921@mydavinci.nl>
2017-03-23 06:26:52 +0100

    show group name, can add 1 user to group

--- application/views/group/add_users.php
+++ application/views/group/add_users.php
@@ -10,11 +10,13 @@
   <a class="nav-link disabled" href="#">Disabled</a>
 </nav>
 	<h1>Leerlingen toevoegen</h1>
-
+  <h2>Groep naam:</h2>
+  <h3><b><?php echo set_value('name'); ?></b></h3>
 <div class="row">
   <div class="col-lg-4">
     <div class="input-group">
       <input type="text" class="form-control" name = "username" placeholder="Leerling naam">
+      <input type="hidden" name="groupname" value="<?php echo set_value('name');?>" />
       <span class="input-group-btn">
         <button class="btn btn-default" name="submit" type="submit ">Voeg toe!</button>
       </span>

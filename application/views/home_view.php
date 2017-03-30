40bf8f96587232e0fd817eff69933d13528dfd33
AbdullahMelihYurtkap <99017921@mydavinci.nl>
2017-03-30 16:05:45 +0200

    edited some layout and started with groups_view on the adminpanel

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,21 +1,9 @@
-
 <div class="jumbotron">
-
-<div class="collapse navbar-collapse" id="navbar1">
-      <ul class="nav navbar-nav navbar-right">
-        <?php if ($this->session->userdata('logged_in')){ ?>
-        
-        <?php } else { ?>
-        <li><a href="<?php echo base_url(); ?>index.php/login">Login</a></li>
-        <li><a href="<?php echo base_url(); ?>index.php/signup">Signup</a></li>
-        <?php } ?>
-      </ul>
-    </div>
 
     <title>Home</title>
         
         <nav class="navbar navbar-default navbar-static-top">
-  <div class="container-fluid">
+
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -24,50 +12,19 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href=<?php echo base_url(); ?>index.php/home/>Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">  
       <ul class="nav navbar-nav navbar-right">
       <li><a href="<?php echo base_url(); ?>index.php/home/index"<p>Hello <?php echo $this->session->userdata('uname'); ?></p></a>
         <li><a href="<?php echo base_url(); ?>index.php/home/logout">Uitloggen</a></li>
       </ul>
-    </div><!-- /.navbar-collapse -->
+
   </div><!-- /.container-fluid -->
   <div class="container"  style="margin-top:20px;">
 
-
-    <div class="panel panel-default">
-    <!-- Default panel contents -->
-    <div class="panel-heading">Alle groepen</div>
-    <div class="panel-body">
-      <a href="<?php echo base_url();?>index.php/Home/Delete_All_Groups/>">Delete groups</a>
-      <a href="<?php echo base_url();?>index.php/Home/Delete_All_Users/>">Delete users</a>
-    </div>
-
-    <!-- Table -->
-    <table class="table">
-      <tr>  
-          <td><span style="font-weight:bold">Groups</span></td> 
-              </tr>     
-                <?php  
-                 foreach ($getgroup as $row)  
-               {  
-               ?>
-               <tr> 
-                <td><?php echo $row->name;?></td>  
-                <td><a href="<?php echo base_url();?>index.php/Home/Delete/<?php echo $row->id?>">Delete</a></td>
-              </tr>  
-               <?php } ?> 
-    </table>
-  </div>
-  </div>
+</div>
 
           
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,14 +1,14 @@
 
-
 </div>
-
 	<div class="jumbotron jumbotron-influid">
-		<p class="lead">copyrights © 2017 TechniekDock</p> 
+		<p class="navbar-brand">copyrights © 2017 TechniekDock</p> 
   	</div>
 
   	<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+
+
 
 
   
